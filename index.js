fc5794ae980494372e60cd190c8f65ac7db6c18c
MRHemel <hemelrahman80@gmail.com>
2023-07-22 07:20:56 +0600

    database connection

--- index.js
+++ index.js
@@ -33,6 +33,11 @@ async function run() {
             res.send(result)
 
         })
+        app.get('/instructors', async (req, res) => {
+            const result = await instructorsCollection.find().toArray();
+            res.send(result)
+
+        })
 
 
 
