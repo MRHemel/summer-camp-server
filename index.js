53178b6a49726d45880f99c068f2285f145d129a
MRHemel <hemelrahman80@gmail.com>
2023-07-22 07:21:47 +0600

    instructor data pulling from database

--- index.js
+++ index.js
@@ -33,11 +33,6 @@ async function run() {
             res.send(result)
 
         })
-        app.get('/instructors', async (req, res) => {
-            const result = await instructorsCollection.find().toArray();
-            res.send(result)
-
-        })
 
 
 
